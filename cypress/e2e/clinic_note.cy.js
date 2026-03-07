f4421df84144401b4f86ab2a964e1a18e6370111
khaelapus <jude.micheal@livro.ph>
2026-03-07 09:59:51 +0800

    Update clinic_note.cy.js
    Finally solved the previous blocker.

--- cypress/e2e/clinic_note.cy.js
+++ cypress/e2e/clinic_note.cy.js
@@ -7,18 +7,17 @@ describe('Clinic Note', () => {
     cy.get('#login_password').type('Khaelapus_1027')
     cy.get('.btn-login').click()
     cy.wait(1000)
+
     cy.get('div[app-route="/app"]').click()
     cy.wait(2000)
-    //cy.get('.sidebar-toggle-btn').click()
-    //cy.wait(2000)
+
     cy.get('div[item-name="Clinic"]').click()
-    cy.wait(3000)
-    cy.xpath('/html/body/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div[2]/div[1]/div[2]/div/div/div[1]/div[2]/div/div/div/div[2]/custom-block-aoofo//div/div/div[2]/div[1]').click()
+    cy.wait(1000)
+    cy.get('.widget-body', { timeout: 20000 }).children().eq(1).shadow().find('.dash-card').eq(0).click()
     cy.wait(2000)
-    //cy.get('.widget-body')
-    //cy.contains('div', 'Clinic Note').click()
-    //cy.wait(2000)
-    //cy.get('button[data-label="Add Clinic Note"]').click()
-    //cy.wait(2000)
+    cy.get('button[data-label="Add Clinic Note"]').click()
+    cy.wait(2000)
+
+    
   })
 })
