9cf671082cfe422bae5214acfe95b9167569c320
khaelapus <jude.micheal@livro.ph>
2026-03-09 08:12:17 +0800

    updated clinic_note.cy.js
    Finished the flow.

--- cypress/e2e/clinic_note.cy.js
+++ cypress/e2e/clinic_note.cy.js
@@ -18,6 +18,25 @@ describe('Clinic Note', () => {
     cy.get('button[data-label="Add Clinic Note"]').click()
     cy.wait(2000)
 
-    
+    cy.get('input[data-fieldname="quarter"]').clear().type('First Quarter')
+    const d = new Date()
+    const date = `${d.getMonth()+1}-${d.getDate()}-${d.getFullYear()}`
+    const time = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    cy.get('input[data-fieldname="note_date"]').type(date, { force: true }).type('{enter}')   
+    cy.get('input[data-fieldname="time_of_visit"]').type(time).type('{enter}')
+    cy.get('div[data-fieldname="__column_1"]').click()
+    cy.get('select[data-fieldtype="Select"]').eq(0).select('Student')
+    cy.get('input[data-fieldname="student"]').type('TEST, QA').wait(1000).type('{enter}')
+
+    //cy.get('input[data-fieldname="visit_reason"]').type('Felt sick. Had headaches.')
+    cy.get('select[data-fieldtype="Select"]').eq(1).select('Sent Home')
+    cy.get('textarea[data-fieldtype="Small Text"]').eq(0).type('Was given paracetamol by the nurse. Asked parent to pick them up to go home and get rest.')
+    cy.get('input[data-fieldname="contact_numbers"]').type('09155819175')
+    cy.get('button[data-label="Save"]').click()
+    cy.wait(2000)
+    cy.get('a[href="/app/clinic-note"]').click()
+    //cy.get('.flex.fill-width.title-area').should('have.text','Clinic Note')
+    cy.url().should('include', '/clinic-note')
+
   })
 })
